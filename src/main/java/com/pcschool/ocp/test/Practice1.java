fe075b6b618cbffe4152e931ade1b0c64a0b9164
MB-study <MB-study@192.168.1.109>
2020-08-11 15:12:34 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/test/Practice1.java
@@ -0,0 +1,22 @@
+package com.pcschool.ocp.test;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        int[] x = {10, 20, 30};
+    int sum = x[0] + x[1] + x[2];
+    System.out.println(sum);
+    
+    sum = 0;
+    sum += x[0];
+    sum += x[1];
+    sum += x[2];
+        System.out.println(sum);
+        
+        sum = 0;
+        for(int i=1;i<x.length;i++) {
+            sum += x[i];
+    }
+    
+    System.out.println(sum);
+    }
+}
