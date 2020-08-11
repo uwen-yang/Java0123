4b135ae51881016eac64c508dca1e99802d1777f
MB-study <MB-study@192.168.1.109>
2020-08-11 11:42:26 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d01/ATM.java
@@ -0,0 +1,10 @@
+package com.pcschool.ocp.d01;
+
+public class ATM {
+    public static void main(String[] args) {
+        run();
+    }
+    public static void run() {
+        System.out.println("run....");
+    }
+}
