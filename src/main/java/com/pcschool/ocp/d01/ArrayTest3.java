a571ffd03352224912c67df00e72844f965fac35
MB-study <MB-study@192.168.1.109>
2020-08-11 15:28:34 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d01/ArrayTest3.java
@@ -0,0 +1,26 @@
+package com.pcschool.ocp.d01;
+
+import java.util.Arrays;
+
+public class ArrayTest3 {
+    public static void main(String[] args) {
+        double[] h = {172, 168, 164, 170, 176};
+        //Java 7
+        double avg = 0;
+        double sum = 0;
+        for(int i=0, len=h.length;i<len;i++) {
+            sum += h[i];
+        }
+        avg = sum/h.length;
+        System.out.printf("平均: %.2f cm\n", avg);
+        //Java 8
+        Arrays.stream(h).forEach(x -> System.out.println(x));
+        
+        sum = Arrays.stream(h).sum();
+        avg = Arrays.stream(h).average().getAsDouble();
+        System.out.printf("總身高: %.1f 平均身高: %.1f\n", sum, avg);
+        
+        
+        
+    }
+}
