c7b89746894e023b014808d1bca884d836365c93
MB-study <MB-study@192.168.1.109>
2020-08-11 13:25:12 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d01/ReferenceTest.java
@@ -0,0 +1,10 @@
+package com.pcschool.ocp.d01;
+
+public class ReferenceTest {
+    public static void main(String[] args) {
+        String s1 = new String("Java");
+        String s2 = s1;
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
