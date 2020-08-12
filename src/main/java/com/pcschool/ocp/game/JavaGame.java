0d20d61a018e49e18ea538434254bf886128ea86
MB-study <MB-study@192.168.1.109>
2020-08-12 10:36:11 +0800

    Java

--- src/main/java/com/pcschool/ocp/game/JavaGame.java
+++ src/main/java/com/pcschool/ocp/game/JavaGame.java
@@ -5,6 +5,37 @@ import java.util.Scanner;
 public class JavaGame {
 
     public static void main(String[] args) {
+        menu();
+    }
+
+    public static void menu() {
+        while (true) {
+            System.out.println("Java Game");
+            System.out.println("--------------");
+            System.out.println("1.猜數字");
+            System.out.println("2.猜英文");
+            System.out.println("9.Exit");
+            System.out.println("--------------");
+            System.out.println("請選擇 ==> ");
+            int choice = new Scanner(System.in).nextInt();
+            switch (choice) {
+                case 1:
+                    numberGameLoop();
+                    break;
+                case 2:
+                    englishGameLoop();
+                    break;
+                case 9:
+                    return;
+            }
+            System.out.println("請按下 Enter 鍵繼續 ...");
+            new Scanner(System.in).nextLine();
+            
+        }
+
+    }
+
+    public static void englishGameLoop() {
         Number number = new Number();
         while (true) {
             Scanner sc = new Scanner(System.in);
@@ -18,4 +49,23 @@ public class JavaGame {
             }
         }
     }
+
+    public static void numberGameLoop() {
+        English english = new English();
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("請輸入英文字母: ");
+            int userAns = sc.next().toUpperCase().charAt(0);
+            int result = english.check(userAns);
+            if (result == 0) {
+                System.out.println("恭喜答對了~");
+                break;
+            } else {
+                System.out.printf("猜第 %d 次, 結果: %s 了\n", english.getCount(), result > 0 ? "大" : "小");
+            }
+
+        }
+
+    }
+
 }
