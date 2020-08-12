6f0940c946ba019167766f77bb9c5f2554da9ade
MB-study <MB-study@192.168.1.109>
2020-08-12 15:11:42 +0800

    Java

--- src/main/java/com/pcschool/ocp/d02/MultiArray5.java
+++ src/main/java/com/pcschool/ocp/d02/MultiArray5.java
@@ -7,7 +7,8 @@ public class MultiArray5 {
                           {'X','O',' '},
                           {'X',' ','X'}
                         };
-        ttt[2][1] ='O';
+        int x = 7;
+        ttt[x/3][x%3] ='O';
         System.out.println("O 是勝利");
         //印出最後結局
         for(int i=0;i<ttt.length;i++) {
