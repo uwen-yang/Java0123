47c7977478715e2398745a6bba46241f7a2e8358
MB-study <MB-study@192.168.1.109>
2020-08-12 11:12:55 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/game/GameService.java
@@ -0,0 +1,67 @@
+package com.pcschool.ocp.game;
+
+import java.util.Scanner;
+
+
+public class GameService {
+    public static void start() {
+    while (true) {
+            System.out.println("Java Game");
+            System.out.println("--------------");
+            System.out.println("1.猜數字");
+            System.out.println("2.猜英文");
+            System.out.println("9.Exit");
+            System.out.println("--------------");
+            System.out.println("請選擇 ==> ");
+            int choice = new Scanner(System.in).nextInt();
+            switch (choice) {
+                case 1:
+                    numberGameLoop();
+                    break;
+                case 2:
+                    englishGameLoop();
+                    break;
+                case 9:
+                    return;
+            }
+            System.out.println("請按下 Enter 鍵繼續 ...");
+            new Scanner(System.in).nextLine();
+            
+        }
+
+    }
+
+    public static void numberGameLoop() {
+        Number number = new Number();
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("請輸入數字: ");
+            int userAnt = sc.nextInt();
+            int result = number.check(userAnt);
+            if (result == 0) {
+                System.out.println("恭喜答對了~");
+            } else {
+                System.out.printf("猜第 %d 次, 結果: %s 了\n", number.getCount(), result > 0 ? "大" : "小");
+            }
+        }
+    }
+
+    public static void englishGameLoop() {
+        English english = new English();
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("請輸入英文字母: ");
+            int userAns = sc.next().toUpperCase().charAt(0);
+            int result = english.check(userAns);
+            if (result == 0) {
+                System.out.println("恭喜答對了~");
+                break;
+            } else {
+                System.out.printf("猜第 %d 次, 結果: %s 了\n", english.getCount(), result > 0 ? "大" : "小");
+            }
+
+        }
+
+    }
+
+}
