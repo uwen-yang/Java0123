35159269e90dfe34fea75b72f130b3ddd6a2e4b8
MB-study <MB-study@192.168.1.109>
2020-08-12 13:42:08 +0800

    Java

--- src/main/java/com/pcschool/ocp/d02/MulitArray2.java
+++ src/main/java/com/pcschool/ocp/d02/MulitArray2.java
@@ -3,15 +3,12 @@ package com.pcschool.ocp.d02;
 public class MulitArray2 {
 
     public static void main(String[] args) {
-        int[][] x = new int[][]{{100, 90, 80}, {70, 60, 50}};//可不打new int[][]
+        int[][] x = new int[][]{{100, 90, 80}, {70, 60, 50}, {30, 20}};//可不打new int[][]
         System.out.println("x length = " + x.length);
-        //將 x[0] 的內容取出
-        for(int i = 0; i < x[0].length; i++) {
-            System.out.println(x[0][i]);
-
-        }
-        for(int i = 0; i < x[1].length; i++) {
-            System.out.println(x[1][i]);
+        for (int k = 0; k < x.length; k++) {
+            for (int i = 0; i < x[k].length; i++) {
+                System.out.println(x[k][i]);
+            }
         }
     }
 }
