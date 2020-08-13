c75ffa446c0d1e021f954ffb76180b57b7e2903e
MB-study <MB-study@192.168.1.109>
2020-08-13 10:42:33 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/game/case3/Poker.java
@@ -0,0 +1,24 @@
+
+package com.pcschool.ocp.game.case3;
+
+public class Poker {
+    public String mark; // 花色
+    public String number; // 號碼
+    public String color; // 顏色
+    public int    value; // 價值
+    
+    // 建構式
+
+    public Poker(String mark, String number, String color, int value) {
+        this.mark = mark;
+        this.number = number;
+        this.color = color;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return   mark + number;
+    }
+    
+}
---
+++ src/main/java/com/pcschool/ocp/game/case3/PokerMain2.java
@@ -0,0 +1,34 @@
+package com.pcschool.ocp.game.case3;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class PokerMain2 {
+
+    public static void main(String[] args) throws Exception {
+        File f = new File("src\\main\\java\\com\\pcschool\\ocp\\game\\case3\\poker.txt");
+        //通過Scanner抓取檔案內容
+        Scanner sc = new Scanner(f);
+        //將檔案內容全部
+        String allData = sc.useDelimiter("\\A").next();
+        //列印
+        //System.out.println(allData);
+        //透過split()切分資料
+        String[] rows = allData.split("\n");
+        //抓第一筆
+        System.out.println(rows[0]);
+        //分析出每一個欄位
+        String[] row = rows[0].split(",");
+        String mark = row[0];
+        String number = row[1];
+        String color = row[2];
+        int value = Integer.parseInt(row[3]);
+        System.out.printf("mark: %s number: %s color: %s value: %d\n", mark, number, color, value);
+        //建立第一個Poker物件
+        Poker p1 = new Poker(mark, number, color, value);
+        System.out.println(p1.mark);
+        System.out.println(p1.color);
+        System.out.println(p1.number);
+        System.out.println(p1.value);
+    }
+}
