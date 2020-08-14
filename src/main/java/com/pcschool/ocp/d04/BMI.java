9ba6949042c5b6b00424db56a81b2e7ff1c87ffe
MB-study <MB-study@192.168.1.109>
2020-08-14 10:33:16 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d04/BMI.java
@@ -0,0 +1,30 @@
+package com.pcschool.ocp.d04;
+
+public class BMI {
+
+    private double h = 170;//100-250cm(170.0)
+    private double w = 60;//30-150kg (60.0 )
+
+    public BMI() {
+    }
+
+    public BMI(double h, double w) {
+        this(h);
+        if(w >= 30 && w <= 150) {
+            this.w = w;
+        }
+    }
+    
+
+    public BMI(double h) {
+        if(h >= 100 && h <= 250){
+            this.h = h;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "BMI{" + "h=" + h + ", w=" + w + '}';
+    }
+
+}
