1e20633634438ba5c43e644482cf8489a72c17b0
MB-study <MB-study@192.168.1.109>
2020-08-14 13:47:41 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/uml/Dog.java
@@ -0,0 +1,29 @@
+package com.pcschool.ocp.uml;
+
+import java.util.Stack;
+
+public class Dog {
+
+    private String name;
+
+    private int age = 10;
+
+    public static String color = "orange";
+
+    public Dog() {
+    }
+
+    public Dog(String Name, int age) {
+    }
+
+    public Dog(String name) {
+    }
+
+    public void printDog() {
+    }
+
+    public Dog[] findDog(int age) {
+        return null;
+    }
+
+}
