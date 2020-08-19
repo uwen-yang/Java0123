0ceaf04cc0997b2666eeb4287a0252ee1ab500fa
MB-study <MB-study@192.168.1.109>
2020-08-19 14:26:53 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d08/aray/FlatMap2.java
@@ -0,0 +1,17 @@
+package com.pcschool.ocp.d08.aray;
+
+public class FlatMap2 {
+    public static void main(String[] args) {
+        String[][] names = {
+            {"Jonn","Marry"},
+            {"Java","Python","VB"},
+            {"Android","iPhone"},
+        };
+    }
+    //請問總共有幾個字母?
+    
+    //長度最長的是幾個字?
+    
+    //長度最短的幾個字?
+    
+}
