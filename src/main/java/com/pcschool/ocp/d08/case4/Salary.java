baa805576715918e2c6ed9a20ce2e69a4fbdd90d
MB-study <MB-study@192.168.1.109>
2020-08-19 13:35:30 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d08/case4/Salary.java
@@ -0,0 +1,16 @@
+package com.pcschool.ocp.d08.case4;
+
+public class Salary {
+ 
+	private int money;
+	 
+	public Salary(int salary) {
+            this.money = money;
+	}
+	 
+	public int getMoney() {
+		return money;
+	}
+	 
+}
+ 
