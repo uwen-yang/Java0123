b6ba28071abee959533f599a05a2603f85d64df5
MB-study <MB-study@192.168.1.109>
2020-08-19 14:18:20 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/test/FlatMap.java
@@ -0,0 +1,30 @@
+package com.pcschool.ocp.test;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class FlatMap {
+    public static void main(String[] args) {
+        int[][] scores = {
+            {100, 90, 80},
+            {70, 60, 50}
+        };
+        //多種寫法
+        Arrays.stream(scores)
+                .flatMapToInt(IntStream::of); 
+        Arrays.stream(scores)
+                .flatMapToInt(s -> Arrays.stream(s));
+        Arrays.stream(scores)
+                .flatMapToInt(Arrays::stream);
+        Arrays.stream(scores)
+                .flatMapToInt(s -> IntStream.of(s));
+       //---------------------------------------------------
+       //多種寫法
+        Arrays.stream(scores)
+                .flatMapToInt(s -> Arrays.stream(s))
+                .forEach(s -> System.out.println(s));
+        Arrays.stream(scores)
+                .flatMapToInt(Arrays::stream)
+                .forEach(System.out::println);
+    }
+}
