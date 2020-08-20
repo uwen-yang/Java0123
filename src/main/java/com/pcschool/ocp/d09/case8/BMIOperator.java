0c335f97dc3e1f961692926b363aa68788a1f623
MB-study <MB-study@192.168.1.109>
2020-08-20 15:06:56 +0800

    Java

--- src/main/java/com/pcschool/ocp/d09/case8/BMIOperator.java
+++ src/main/java/com/pcschool/ocp/d09/case8/BMIOperator.java
@@ -1,14 +1,20 @@
 package com.pcschool.ocp.d09.case8;
 
+// Java 8 Function API
+// https://docs.oracle.com/javase/8/docs/api/java/util/function/package-summary.html
+
+
 import com.sun.org.apache.xpath.internal.operations.UnaryOperation;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
 import java.util.function.DoubleUnaryOperator;
 import java.util.function.Function;
+import java.util.function.ObjDoubleConsumer;
+import java.util.function.ObjIntConsumer;
 import java.util.function.UnaryOperator;
 
-public class BMIConsumer {
+public class BMIOperator {
     public static void main(String[] args) {
         BiConsumer<Double, Integer> bc = (h, w) -> System.out.println(w / Math.pow(h/100, 2));
         bc.accept(170.0, 65);
@@ -32,5 +38,19 @@ public class BMIConsumer {
         System.out.println(f1.apply(0.0));
         System.out.println(uo.apply(0.0));
         System.out.println(du.applyAsDouble(0.0));
+        
+        /////BinaryOperator<Integer>scoreAvg = (chinese, english) -> (chinese+english)/2;
+        /////ObjIntConsumer<BinaryOperator<Integer>> oci = (avg, pass) -> System.out.println(avg.apply(pass, pass));
+        
+        class Exam {
+            int chinese;
+            int english;
+        }
+        Exam exam = new Exam();
+        exam.chinese = 100;
+        exam.english = 80;
+        
+        ObjIntConsumer<Exam> ois = (e, value) -> {System.out.println((e.chinese+e.english)/2>=value);};
+        ois.accept(exam, 60);
     }
 }
