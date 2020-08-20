b4659dd9c2011f1caad29232a5339b804ffc1545
MB-study <MB-study@192.168.1.109>
2020-08-20 09:51:28 +0800

    Java

--- src/main/java/com/pcschool/ocp/d09/case1/Employee.java
+++ src/main/java/com/pcschool/ocp/d09/case1/Employee.java
@@ -2,28 +2,34 @@ package com.pcschool.ocp.d09.case1;
 
 //外部類別
 public class Employee {
+
     String name = "月例會";
+
     class Job {
+
         String name = "Java";
-        
+
         public void program() {
             String name = "寫程式";
             System.out.println(name + ":" + this.name);
         }
-        
+
         public void meeting() {
             String name = "開會";
             System.out.println(name + ":" + this.name);
         }
     }
+
     //一般內部類別
     class Study {
+
         public void read() {
             String appName = "美食 App";
             int hours = 60;
             System.out.println("進修時間:" + hours + "hr");
             //方法內部類別
             class App {
+
                 public void printName() {
                     System.out.println(appName);
                 }
