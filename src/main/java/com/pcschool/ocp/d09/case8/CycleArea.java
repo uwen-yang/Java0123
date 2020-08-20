a64a6032c2b97953cf48fb74ba6394b46279b8f6
MB-study <MB-study@192.168.1.109>
2020-08-20 13:38:00 +0800

    Java

--- src/main/java/com/pcschool/ocp/d09/case8/CycleArea.java
+++ src/main/java/com/pcschool/ocp/d09/case8/CycleArea.java
@@ -1,13 +1,15 @@
 package com.pcschool.ocp.d09.case8;
 
 import java.util.Arrays;
+import java.util.function.Supplier;
 
 public class CycleArea {
     public static void main(String[] args) {
+        Supplier<Double>pi = () -> Math.PI;
         int[] rs = {5, 10, 15, -4, 20, 0, -25};
         Arrays.stream(rs)
                 .filter((r) -> r > 0)//Predicate 得到 5, 10, 15, 20
-                .mapToDouble((r) -> r * r * Math.PI)//Function
+                .mapToDouble((r) -> r * r * pi.get())//Function
                 .forEach(area -> System.out.println(area));//Consimer
                 
     }
