71c6edfea59b34ac4d8cac8cd50e09349fa1fd44
MB-study <MB-study@192.168.1.109>
2020-08-21 11:43:41 +0800

    Java

--- src/main/java/com/pcschool/ocp/d10/case4/Student.java
+++ src/main/java/com/pcschool/ocp/d10/case4/Student.java
@@ -1,9 +1,41 @@
 package com.pcschool.ocp.d10.case4;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class Student {
     private String name;
     private Set<Exam> scores;
-    
+
+    public Student(String name) {//建構式
+        this(name, new LinkedHashSet());//建立一個空的集合
+    }
+
+    public Student(String name, Set<Exam> scores) {
+        this.name = name;
+        this.scores = scores;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Exam> getScores() {
+        return scores;
+    }
+
+    public Set<Exam> addScores(int score) {
+        Exam exam = new Exam(score);
+        scores.add(exam);
+        return scores;
+    }
+
+    @Override
+    public String toString() {//封裝
+        return "Student{" + "name=" + name + ", scores=" + scores + '}';
+    } 
 }
