8c2db81563a6ad8e7c9f20d8c24a655405a4228f
MB-study <MB-study@192.168.1.109>
2020-08-25 10:40:23 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d13_assert/MyObject.java
@@ -0,0 +1,27 @@
+package com.pcschool.ocp.d13_assert;
+
+public class MyObject {
+
+    private String s;
+
+    public MyObject() {
+        String tName = Thread.currentThread().getName();
+        System.out.println(tName + "執行建構子");
+
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        String tName = Thread.currentThread().getName();
+        System.out.println(tName + "執行 main 方法"+ Runtime.getRuntime().freeMemory());
+    }
+
+    public static void main(String[] args) {
+        String tName = Thread.currentThread().getName();
+        System.out.println(tName + "執行 main 方法, freeMemory" + Runtime.getRuntime().freeMemory());
+        MyObject mo = new MyObject();
+        mo = null;
+        System.gc();
+    }
+}
