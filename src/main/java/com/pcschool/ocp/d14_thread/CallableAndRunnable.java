503334f324dc225ae752f38300b4b7c80ab5984e
MB-study <MB-study@192.168.1.109>
2020-08-25 14:31:04 +0800

    Java

--- src/main/java/com/pcschool/ocp/d14_thread/CallableAndRunnable.java
+++ src/main/java/com/pcschool/ocp/d14_thread/CallableAndRunnable.java
@@ -1,17 +1,21 @@
 package com.pcschool.ocp.d14_thread;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CallableAndRunnable {
+
     public static void main(String[] args) {
         Runnable r = () -> {
             System.out.println("在 Runnable 中得到 Java");
-            return;
+            return;//無回傳值
         };
         Callable c = () -> {
             System.out.println("在 Callable 中得到 Python");
-            return "Python";
-            
+            return "Python";//有回傳值
         };
+        ExecutorService service = Executors.newCachedThreadPool();
+        
     }
 }
