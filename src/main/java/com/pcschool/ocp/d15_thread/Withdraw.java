8e59316e89a255f41a247d72b30a17229948a53f
MB-study <MB-study@192.168.1.109>
2020-08-25 16:05:53 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d15_thread/Withdraw.java
@@ -0,0 +1,26 @@
+package com.pcschool.ocp.d15_thread;
+
+import com.pcschool.ocp.d01.Account;
+
+public class Withdraw implements Runnable {
+    //提款工作
+    private Account account;
+    private int money;
+    
+    public Withdraw(Account account, int money) {
+        this.account = account;
+        this.money= money;
+        
+    }
+
+    public Withdraw(com.pcschool.ocp.d15_thread.case1.Account account, int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+   
+    @Override
+    public void run() {
+        account.withdraw(money);
+    }
+    
+}
