00f7a07aafc0b96ebe205e34d161bfa870643200
MB-study <MB-study@192.168.1.109>
2020-08-26 11:34:10 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d16_derby/SelectRecord.java
@@ -0,0 +1,38 @@
+package com.pcschool.ocp.d16_derby;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import javax.naming.spi.DirStateFactory;
+
+public class SelectRecord {
+
+    public static void main(String[] args) throws Exception {
+        String jdbcUrl = "jdbc:derby://localhost:1527/demo";//資料庫的位置在哪裡?
+        String username = "app";
+        String password = "app";
+        Connection conn = DriverManager.getConnection(jdbcUrl, username, password);
+        //建立資料庫連線
+        Statement stmt = conn.createStatement();
+        //執行查詢SQL
+        String sql = "SELECT id, bename, ts FROM book";
+        ResultSet rs = stmt.executeQuery(sql);
+        //將資料輪循後印出
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            String bename = rs.getString("bename");
+            int price = rs.getInt("price");
+            Timestamp ts = rs.getTimestamp("ts");
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss E");
+            System.out.printf("%4d %10s %4d %20s\n", id, bename, price, sdf.format(ts));
+
+        }
+
+        //關閉連線
+        conn.close();
+    }
+}
