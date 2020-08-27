03a0e8cdd221adf810aa198019cac002b1e60907
MB-study <MB-study@192.168.1.109>
2020-08-27 13:03:38 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d17_IO/case5/OutputStreamWrite.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pcschool.ocp.d17_IO.case5;
+
+import java.io.FileOutputStream;
+import java.nio.charset.Charset;
+
+/**
+ *
+ * @author MB-study
+ */
+public class OutputStreamWrite {
+
+    OutputStreamWrite(FileOutputStream fos, Charset UTF_8) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
